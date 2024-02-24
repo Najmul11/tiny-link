461c0f0a302c90c06f28ba00a665e129411d1ec6
Najmul <najmulbinnurul@gmail.com>
2024-02-24 22:05:49 +0600

    header responsive

--- client/components/home-page/Hero.tsx
+++ client/components/home-page/Hero.tsx
@@ -8,15 +8,15 @@ const Hero = () => {
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-      <div className="flex flex-col  text-center items-center w-1/3 mx-auto">
+      <div className="flex flex-col  text-center items-center w-full xl:w-1/3 mx-auto">
         <span className="text-sm text-[#7CDBE9]/80 bg-[#294358]/60 px-5 py-1 rounded-full">
           No credit card required.
         </span>
 
-        <p className="text-4xl sm:text-4xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500  text-center">
+        <p className="text-2xl lg:text-4xl sm:text-2xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500  text-center">
           More than just shorter links
         </p>
-        <p className="text-sm mt-3 text-white/80">
+        <p className="text-sm mt-3 text-white/80 w-96 lg:w-[unset] mx-auto">
           Shorten links using NanoLink super fast. NanoLink is a oneclick paste
           and copy URL shortener which alows you to shorten links lighting fast
         </p>
--- client/components/layouts/Header.tsx
+++ client/components/layouts/Header.tsx
@@ -12,15 +12,12 @@ const Header = () => {
 
   return (
     <div className="bg-black text-white">
-      <div className="max-w-screen-xl mx-auto">
+      <div className="max-w-screen-xl mx-auto px-4 xl:px-[unset]">
         <div className="flex justify-between py-3">
           {/* left */}
           <div className="flex gap-10 items-center">
             <Link href={"/"} className="font-bold text-2xl">
               TINY LINK
-            </Link>
-            <Link href={"/features"} className="text-[14px] font-semibold">
-              Features
             </Link>
           </div>
 
