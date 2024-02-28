9327e3c5669574518c374fdc16e7c74a185d2647
Najmul <najmulbinnurul@gmail.com>
2024-02-28 22:26:21 +0600

    redirect working

--- backend/src/app/modules/links/links.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Model, Types } from 'mongoose';
-
-export type TLink = {
-  originalLink: string;
-  shortLink: string;
-  user: Types.ObjectId;
-};
-
-export type LinkModel = Model<TLink, Record<string, unknown>>;
--- backend/src/app/modules/redirect/redirect.service.ts
+++ backend/src/app/modules/redirect/redirect.service.ts
@@ -1,7 +1,19 @@
-import { Link } from '../links/links.model';
+import httpStatus from 'http-status';
+import { prisma } from '../../../app';
+import ApiError from '../../../errors/ApiError';
 
 const redirectToOriginalLink = async (shortLink: string) => {
-  const link = await Link.findOne({ shortLink });
+  const link = await prisma.link.findFirst({ where: { shortLink } });
+
+  if (!link) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'Link not found');
+  }
+
+  await prisma.link.update({
+    where: { shortLink },
+    data: { clicks: { increment: 1 } },
+  });
+
   return link?.originalLink;
 };
 
