5c21326e9af016bbef262888c07d760eb81dbf9f
Najmul <najmulbinnurul@gmail.com>
2024-03-01 10:32:38 +0600

    privatroute

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,6 +1,5 @@
 import Features from "@/components/home-page/Features";
 import Hero from "@/components/home-page/Hero";
-import { useSession } from "next-auth/react";
 
 export default function Home() {
   return (
